4ba1bef7e82ebace5e977443ff6e46303fa31ddd
UTTAM SHARMA <52076701+sharma-uttam@users.noreply.github.com>
2020-05-11 11:55:56 +0530

    Delete app.js

--- llSPS-INT-942-Intelligent-Customer-Help-Desk-with-Smart-Document-Understanding/app.js
+++
@@ -1,7 +0,0 @@
-
-server.then(app => {
-  app.listen(port, () => {
-    // eslint-disable-next-line no-console
-    console.log('Watson Discovery SDU UI Server running on port: %d', port);
-  });
-});
